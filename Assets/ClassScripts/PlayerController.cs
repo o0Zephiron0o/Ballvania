5ae4b6003f5134ba1e8959278f72dbbd2be60ec3
o0Zephiron0o <70419129+o0Zephiron0o@users.noreply.github.com>
2023-02-02 03:32:17 +0700

    SHIP

--- Assets/ClassScripts/PlayerController.cs
+++ Assets/ClassScripts/PlayerController.cs
@@ -52,11 +52,43 @@ public class PlayerController : MonoBehaviour
         _rb.velocity = moveInput * _moveSpeed;
         //_rb.AddForce(moveInput * _moveSpeed);
 
+        RotateShip();
+
     }
 
     private void OnShoot(InputAction.CallbackContext context)
     {
         //Debug.Log("Shoot");
         Instantiate(_bullet,gameObject.transform.position,Quaternion.identity);
+    }
+
+    private void RotateShip()
+    {
+        Vector2 lookDirection;
+
+        //mouse
+        if(_input.currentControlScheme == _controls.MouseKeyboardScheme.name)
+        {
+            //get mouse input
+            Vector3 mousePos = Mouse.current.position.ReadValue();
+
+            //make the z axis align
+            mousePos.z = _rb.transform.position.z - Camera.main.transform.position.z;
+
+
+            Vector3 Worldpos = (Camera.main.ScreenToWorldPoint(mousePos)- _rb.transform.position).normalized;
+
+            Debug.Log(Worldpos);
+
+
+            _rb.transform.rotation = Quaternion.LookRotation(Vector3.forward,Worldpos);
+
+
+
+            //_rb.transform.rotation = Quaternion.LookRotation( lookDirection);
+        }
+
+
+        //gamepad
     }
 }

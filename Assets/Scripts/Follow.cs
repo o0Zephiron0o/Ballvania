bb3c4eeb62a4618554dddf69708e8c218791cc97
o0Zephiron0o <70419129+o0Zephiron0o@users.noreply.github.com>
2023-05-24 22:59:29 +0700

    Camera

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -6,6 +6,9 @@ public class Follow : MonoBehaviour
 {
     [SerializeField] GameObject target;
 
+    [SerializeField] float xOffset = 1;
+    [SerializeField] float yOffset = 1;
+
     private void Start()
     {
         //target = GameObject.Find("PlayerBall");
@@ -19,6 +22,6 @@ public class Follow : MonoBehaviour
             target = FindObjectOfType<PlayerStats>()?.gameObject;
         }
         
-        gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 5, gameObject.transform.position.z);
+        gameObject.transform.position = new Vector3(target.transform.position.x + xOffset, target.transform.position.y + yOffset, gameObject.transform.position.z);
     }
 }
